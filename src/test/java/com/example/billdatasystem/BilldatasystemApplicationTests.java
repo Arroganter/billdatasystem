54946b0d15f3b7dea208f04c516eaf70522409e7
Arroganter <1903831817@qq.com>
2019-07-29 11:16:31 +0800

    first

---
+++ src/test/java/com/example/billdatasystem/BilldatasystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.billdatasystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BilldatasystemApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
